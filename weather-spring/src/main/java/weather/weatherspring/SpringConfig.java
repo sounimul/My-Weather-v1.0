4cfc0a7bd9a3cba2f942b1801ea7c22b60013271
songu1 <75112062+songu1@users.noreply.github.com>
2023-03-09 02:39:07 +0900

    회원가입 구현

--- weather-spring/src/main/java/weather/weatherspring/SpringConfig.java
+++ weather-spring/src/main/java/weather/weatherspring/SpringConfig.java
@@ -1,7 +1,31 @@
 package weather.weatherspring;
 
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import weather.weatherspring.repository.JpaMemberRepository;
+import weather.weatherspring.repository.MemberRepository;
+import weather.weatherspring.service.MemberService;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+    private final EntityManager em;
+
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new JpaMemberRepository(em);
+    }
 }
