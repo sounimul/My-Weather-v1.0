d146ef093cc675c590f233c75b42380a9bd5666d
Yujeong Song <75112062+songu1@users.noreply.github.com>
2023-04-28 16:20:13 +0900

    Update DML(fake data).sql

--- DB/DML(fake data).sql
+++ DB/DML(fake data).sql
@@ -20,11 +20,12 @@ INSERT INTO WTYPE VALUES ('PTY_6','빗방울눈날림','weather_mix');
 INSERT INTO WTYPE VALUES ('PTY_7','눈날림','weather_snowy');
 
 -- WEATHER_RECORD - (매우춥다-춥다-쌀쌀하다-시원하다-따뜻하다-약간덥다-덥다-매우덥다)
-INSERT INTO WEATHER_RECORD VALUES ('1','2023-02-15 15:35:24','대구광역시 달서구 상인동','SKY_3',5.7,'쌀쌀해요',20,'건조해요',0.0,'안와요');
-INSERT INTO WEATHER_RECORD VALUES ('2','2023-02-23 10:24:36','대구광역시 중구 삼덕동','SKY_1',12.8,'선선해요',40,'쾌적해요',0.0,'안와요');
-INSERT INTO WEATHER_RECORD VALUES ('2','2023-04-20 12:11:14','서울특별시 송파구 석촌동','SKY_4',20.2,'따뜻해요',50,'쾌적해요',0.0,'안와요');
-INSERT INTO WEATHER_RECORD VALUES ('2','2023-04-26 17:01:00','부산광역시 수영구 민락동','PTY_1',18.1,'시원해요',80,'습해요',0.3,'보슬');
-INSERT INTO WEATHER_RECORD VALUES ('10','2023-01-21 11:25:33','서울특별시 서초구 서초동','PTY_2',-2.2,'추워요',70,'습해요',1.0,'추적');
-INSERT INTO WEATHER_RECORD VALUES ('10','2023-01-21 11:25:58','서울특별시 서초구 서초동','PTY_2',-2.2,'추워요',70,'습해요',1.1,'쏟아져요');
+INSERT INTO WEATHER_RECORD VALUES ('1','2023-02-15 15:35:24','2월 15일','대구광역시 달서구 상인동','partly_cloudy_day',5.7,'쌀쌀',20,'건조',0.0,'안와요');
+INSERT INTO WEATHER_RECORD VALUES ('2','2023-02-23 10:24:36','2월 23일','대구광역시 중구 삼덕동','sunny',12.8,'선선',40,'쾌적',0.0,'안와요');
+INSERT INTO WEATHER_RECORD VALUES ('2','2023-04-20 12:11:14','4월 20일','서울특별시 송파구 석촌동','cloudy',20.2,'따뜻',50,'쾌적',0.0,'안와요');
+INSERT INTO WEATHER_RECORD VALUES ('2','2023-04-26 17:01:00','4월 26일','부산광역시 수영구 민락동','rainy',18.1,'시원',80,'습해요',0.3,'보슬');
+INSERT INTO WEATHER_RECORD VALUES ('10','2023-01-21 11:25:33','1월 21일','서울특별시 서초구 서초동','weather_mix',-2.2,'추워요',70,'습해요',1.0,'추적');
+INSERT INTO WEATHER_RECORD VALUES ('10','2023-01-21 11:25:58','1월 21일','서울특별시 서초구 서초동','weather_mix',-2.2,'추워요',70,'습해요',1.1,'쏟아짐');
+
 
 COMMIT;
