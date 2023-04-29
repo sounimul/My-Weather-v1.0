c0005c2c6cbceef7edacf7b5f2a4d1b19285e8d9
songu1 <75112062+songu1@users.noreply.github.com>
2023-04-29 21:42:48 +0900

    프로필 수정 및 비밀번호 변경 구현

--- weather-spring/src/main/java/weather/weatherspring/controller/MypageController.java
+++ weather-spring/src/main/java/weather/weatherspring/controller/MypageController.java
@@ -1,16 +1,21 @@
 package weather.weatherspring.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import weather.weatherspring.domain.Member;
+import weather.weatherspring.entity.MemberForm;
 import weather.weatherspring.repository.MemberRepository;
 import weather.weatherspring.repository.RecordRepository;
+import weather.weatherspring.service.MemberService;
 import weather.weatherspring.service.RecordService;
 
+import java.io.PrintWriter;
 import java.util.List;
 
 
@@ -19,11 +24,14 @@ public class MypageController {
     @Autowired
     private HttpServletRequest request;
     @Autowired
+    private final MemberService memberService;
+    @Autowired
     private final MemberRepository memberRepository;
     @Autowired
     private final RecordService recordService;
 
-    public MypageController(MemberRepository memberRepository, RecordService recordService) {
+    public MypageController(MemberService memberService, MemberRepository memberRepository, RecordService recordService) {
+        this.memberService = memberService;
         this.memberRepository = memberRepository;
         this.recordService = recordService;
     }
@@ -43,5 +51,48 @@ public class MypageController {
         return "myPage";
     }
 
+    @GetMapping ("/changePwform")
+    public String pwForm(){
+        return "passwordChange.html";
+    }
 
+    @PostMapping("/updateProfile")
+    public String newProfile(MemberForm profileForm){
+        HttpSession session = request.getSession();
+        Long uid=(Long) session.getAttribute("uid");
+
+        Member member = memberService.updateProfile(uid,profileForm).get();
+
+        System.out.println(member.getNickname());
+
+        return "redirect:/myPage";
+    }
+
+    @PostMapping("/update-pw")
+    public void changePassword(MemberForm pwForm, HttpServletResponse response){
+        HttpSession session = request.getSession();
+        Long uid=(Long) session.getAttribute("uid");
+        response.setContentType("text/html; charset=utf-8");
+
+        if(memberService.updatePw(uid,pwForm)) {
+            try {
+                PrintWriter w = response.getWriter();
+                w.write("<script>alert('비밀번호가 변경되었습니다.');window.close();</script>");
+                w.flush();
+                w.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            try {
+                PrintWriter w = response.getWriter();
+                w.write("<script>alert('기존 비밀번호가 일치하지 않습니다.');history.go(-1);</script>");
+                w.flush();
+                w.close();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
