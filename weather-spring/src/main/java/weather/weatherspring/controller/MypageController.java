a800841576180eb7047d3d9e50693df4cf793d9f
songu1 <75112062+songu1@users.noreply.github.com>
2023-05-04 21:23:27 +0900

    myPage에서 조건 검색 키워드 Controller로 넘기기 구현

--- weather-spring/src/main/java/weather/weatherspring/controller/MypageController.java
+++ weather-spring/src/main/java/weather/weatherspring/controller/MypageController.java
@@ -17,6 +17,7 @@ import weather.weatherspring.service.RecordService;
 
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -33,26 +34,29 @@ public class MypageController {
         this.recordService = recordService;
     }
 
-    @GetMapping("/myPage/search")
-    public String myPageSearch(Model model, @RequestParam("temp") String temp, @RequestParam("humid") String humid, @RequestParam("prep") String prep){
+    @GetMapping("/myPage")
+    public String myPage(Model model, @RequestParam("temp") Optional<String> temp, @RequestParam("humid") Optional<String> humid, @RequestParam("prep") Optional<String> prep){
         HttpSession session = request.getSession();
         Long uid=(Long) session.getAttribute("uid");
         Search search=new Search();
         String[] value;
         // 받아온 검색조건 string을 숫자로 변환하여 search 객체에 넣기
-        if(temp!=null){
-            value = temp.split(",");
-            search.setStartTemp(Double.parseDouble(value[0])); search.setEndTemp(Double.parseDouble(value[1]));
-            System.out.println(search.getStartTemp());
-            System.out.println(search.getEndTemp());
-        }
-        if(humid!=null){
-            value=humid.split(",");
-            search.setStartHumid(Integer.parseInt(value[0])); search.setEndHumid(Integer.parseInt(value[1]));
-        }
-        if(prep!=null){
-            value=prep.split(",");
-            search.setStartPrep(Double.parseDouble(value[0])); search.setEndPrep(Double.parseDouble(value[1]));
+        if(temp.isPresent() & humid.isPresent() & prep.isPresent()){
+            if(!temp.get().equals("-1")){
+                value = temp.get().split("n");
+                search.setStartTemp(Double.parseDouble(value[0])); search.setEndTemp(Double.parseDouble(value[1]));
+                System.out.println("temp: "+search.getStartTemp()+" "+search.getEndTemp());
+            }
+            if(!humid.get().equals("-1")){
+                value=humid.get().split("n");
+                search.setStartHumid(Integer.parseInt(value[0])); search.setEndHumid(Integer.parseInt(value[1]));
+                System.out.println("humid: "+search.getStartHumid()+" "+search.getEndHumid());
+            }
+            if(!prep.get().equals("-1")){
+                value=prep.get().split("n");
+                search.setStartPrep(Double.parseDouble(value[0])); search.setEndPrep(Double.parseDouble(value[1]));
+                System.out.println("prep: "+search.getStartPrep()+" "+search.getEndPrep());
+            }
         }
 
         List<Record> records = recordService.findRecordList(uid);
@@ -65,20 +69,20 @@ public class MypageController {
         return "myPage";
     }
 
-    @GetMapping("/myPage")
-    public String myPage(Model model){
-        HttpSession session = request.getSession();
-        Long uid=(Long) session.getAttribute("uid");
-
-        List<Record> records = recordService.findRecordList(uid);
-//        List<Record> records = recordService.findRecordList(uid,search);
-        Member member= memberService.findMember(uid).get();
-
-        model.addAttribute("user",member);
-        model.addAttribute("records",records);
-
-        return "myPage";
-    }
+//    @GetMapping("/myPage")
+//    public String myPage(Model model){
+//        HttpSession session = request.getSession();
+//        Long uid=(Long) session.getAttribute("uid");
+//
+//        List<Record> records = recordService.findRecordList(uid);
+////        List<Record> records = recordService.findRecordList(uid,search);
+//        Member member= memberService.findMember(uid).get();
+//
+//        model.addAttribute("user",member);
+//        model.addAttribute("records",records);
+//
+//        return "myPage";
+//    }
 
 
 
