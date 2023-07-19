a1d9ddaf9cf6584da9507b6cfb9547416ae87df4
Yujeong Song <75112062+songu1@users.noreply.github.com>
2023-07-19 19:28:32 +0900

    Create DML.sql

---
+++ DB/DML.sql
@@ -0,0 +1,12 @@
+INSERT INTO wtype VALUES ('SKY_1','맑음','sunny');
+INSERT INTO wtype VALUES ('SKY_3','구름 많음','partly_cloudy_day');
+INSERT INTO wtype VALUES ('SKY_4','흐림','cloudy');
+INSERT INTO wtype VALUES ('PTY_1','비','rainy');
+INSERT INTO wtype VALUES ('PTY_2','비 또는 눈','weather_mix');
+INSERT INTO wtype VALUES ('PTY_3','눈','weather_snowy');
+INSERT INTO wtype VALUES ('PTY_4','소나기','rainy_heavy');
+INSERT INTO wtype VALUES ('PTY_5','빗방울','rainy');
+INSERT INTO wtype VALUES ('PTY_6','빗방울눈날림','weather_mix');
+INSERT INTO wtype VALUES ('PTY_7','눈날림','weather_snowy');
+
+COMMIT;
